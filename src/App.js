271da8556097482159d6a0073106528bc689522d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-21 14:22:07 +0100

    style component box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import data from './data'
+import Box from './Box'
 
 function App() {
   
-  const allBoxes = data.map(singleBox => <Box className='box'></Box>)
+  const allBoxes = data.map(singleBox => <Box />)
 
   return (
     <div>
