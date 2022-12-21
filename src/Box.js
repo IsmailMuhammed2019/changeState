33ee767dc6fc909d095920ce06bf2a66809ce750
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-21 14:29:47 +0100

    add onClick props

--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const Box = (props) => {
+
 const styles = {
  backgroundColor: props.on ? "#222222" : "transparent"
 }
 
   return (
-    <div className='box' style={styles}></div>
+    <div 
+    className='box' 
+    style={styles}
+    onClick={() => (props.handle)}
+    ></div>
   )
 }
 
