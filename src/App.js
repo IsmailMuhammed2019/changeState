5e24ff883779c5c2d1d9805d7e3a45c71770a737
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-21 12:32:08 +0100

    creating boxes component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import data from './data'
 
 function App() {
+  
+  const allBoxes = data.map(singleBox => <div className='box'></div>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          this is my first app
-        </a>
-      </header>
+    <div>
+      {allBoxes}
     </div>
   );
 }
